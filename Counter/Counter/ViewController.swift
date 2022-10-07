f3d28c70707ac765b405da33fe61d39b44e57c79
Shliakhin Sergei <sshlyahin@yahoo.com>
2022-10-07 20:07:37 +0300

    feat: Connect to code

--- Counter/Counter/ViewController.swift
+++ Counter/Counter/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - Outlets
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    //MARK: - Actions
+    @IBAction func buttonTaped(_ sender: UIButton) {
+    }
+    
 }
 
