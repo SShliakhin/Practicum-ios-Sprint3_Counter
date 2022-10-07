337fb6edde77a48df19d8a73baced98cdf7af04a
Shliakhin Sergei <sshlyahin@yahoo.com>
2022-10-07 20:13:25 +0300

    feat: Add logic

--- Counter/Counter/ViewController.swift
+++ Counter/Counter/ViewController.swift
@@ -12,12 +12,27 @@ class ViewController: UIViewController {
     //MARK: - Outlets
     @IBOutlet weak var label: UILabel!
     
+    //MARK: - Properties
+    private var counter = 0 {
+        didSet {
+            label.text = "Значение счетчика: \(counter)"
+        }
+    }
+    
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+    }
+    
+    //MARK: - Methods
+    private func setup() {
+        counter = 0
     }
     
     //MARK: - Actions
     @IBAction func buttonTaped(_ sender: UIButton) {
+        counter += 1
     }
     
 }
