bfdd659e5ad817faaa534cbac41db07b87b786e9
Sergei Shliakhin <71236551+SShliakhin@users.noreply.github.com>
2022-10-09 19:14:34 +0300

    Solve without storyboard (#3)
    * feat: Implement no storyboard
    
    * feat: Implement UI with autolayout
    
    * feat: Add logic

--- Counter/Counter/ViewController.swift
+++ Counter/Counter/ViewController.swift
@@ -8,9 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    //MARK: - Outlets
-    @IBOutlet weak var label: UILabel!
+    
+    let stackView = UIStackView()
+    let label = UILabel()
+    let button = UIButton()
     
     //MARK: - Properties
     private var counter = 0 {
@@ -23,17 +24,57 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
+        style()
+        layout()
     }
-    
-    //MARK: - Methods
-    private func setup() {
-        counter = 0
-    }
-    
-    //MARK: - Actions
-    @IBAction func buttonTaped(_ sender: UIButton) {
-        counter += 1
-    }
-    
 }
 
+//MARK: - Private Methods
+extension ViewController {
+    private func setup() {
+        counter = 0
+        button.addTarget(self, action: #selector(buttonTapped), for: .primaryActionTriggered)
+    }
+    
+    private func style() {
+        view.backgroundColor = .systemBackground
+        
+        stackView.axis = .vertical
+        stackView.spacing = 40
+
+        label.textAlignment = .center
+        label.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        label.numberOfLines = 0
+        
+        button.setTitle("Увеличить счетчик", for: .normal)
+        button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.backgroundColor = .systemGray
+    }
+    
+    private func layout() {
+        [ label,
+          button
+        ].forEach { item in
+            item.translatesAutoresizingMaskIntoConstraints = false
+            stackView.addArrangedSubview(item)
+        }
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.safeAreaLayoutGuide.leadingAnchor, multiplier: 2),
+            view.safeAreaLayoutGuide.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 2),
+        ])
+    }
+}
+
+//MARK: - Actions
+extension ViewController {
+    @objc func buttonTapped () {
+        counter += 1
+    }
+}
